7973284b113e35d5e372dd74c856dc6dae63fa30
shivams334 <shivams334@gmail.com>
2016-11-01 14:00:00 +0530

    did some extra calculations,deleted media files,extra code removed

--- hack.sh
+++ hack.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 list=("added index file","updated readme file", "did some extra calculations","deleted media files","extra code removed")
-for i in {1..320}
+for i in {1..90}
 do
-	touch temp$i.py
+	touch temp$i.txt
 	git add -A
-	a="Sun Mar "
+	a="Wed Nov "
 	b=$i
-	c=" 14:00 2017 +0530"
+	c=" 14:00 2016 +0530"
 	export GIT_AUTHOR_DATE=$a$b$c
 	export GIT_COMMITTER_DATE=$a$b$c
 	git commit -am "`echo ${list[$RANDOM % ${#list[@]} ]}`"
+	rm temp$i.txt
 	export GIT_AUTHOR_DATE=$a$b$c
 	export GIT_COMMITTER_DATE=$a$b$c
 	export GIT_AUTHOR_NAME="Shivam"

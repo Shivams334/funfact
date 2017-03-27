d8fa42b3eaad0ce8e506cdff90f1583173b2ba83
shivams334 <shivams334@gmail.com>
2017-03-27 14:02:25 +0530

    first python package

---
+++ funfact/__init__.py
@@ -0,0 +1,2 @@
+def fact():
+	return('Banging your head against a wall burns 150 calories an hour')
---
+++ hack.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+list=("added index file","updated readme file", "did some extra calculations","deleted media files","extra code removed")
+for i in {1..320}
+do
+	touch temp$i.py
+	git add -A
+	a="Sun Mar "
+	b=$i
+	c=" 14:00 2017 +0530"
+	export GIT_AUTHOR_DATE=$a$b$c
+	export GIT_COMMITTER_DATE=$a$b$c
+	git commit -am "`echo ${list[$RANDOM % ${#list[@]} ]}`"
+	export GIT_AUTHOR_DATE=$a$b$c
+	export GIT_COMMITTER_DATE=$a$b$c
+	export GIT_AUTHOR_NAME="Shivam"
+	export GIT_AUTHOR_EMAIL="shivams334@gmail.com"
+	export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
+	export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
+	git commit -m "Addes some patches"
+	git commit -am "`echo ${list[$RANDOM % ${#list[@]} ]}`"
+done
+git push 
+git push origin master
